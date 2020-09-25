3c0ffd7af446f93716f76975d4ef7c228ba87edf
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-25 17:39:43 +0800

    update order

--- front-end/src/components/Order.js
+++ front-end/src/components/Order.js
@@ -6,8 +6,15 @@ class Order extends Component {
 
     render() {
         return (
-            <div className='home'>
-                <p>This is a beautiful order Page.</p>
+            <div className='order'>
+                <ul>
+                    <li>名字</li>
+                    <li>单价</li>
+                    <li>数量</li>
+                    <li>单位</li>
+                    <li>操作</li>
+
+                </ul>
             </div>
         );
     }
