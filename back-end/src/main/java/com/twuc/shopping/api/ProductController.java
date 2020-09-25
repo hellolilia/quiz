dcabcd56d98618c83049f785097663c46ad89fac
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-25 21:59:56 +0800

    add product list

--- back-end/src/main/java/com/twuc/shopping/api/ProductController.java
+++ back-end/src/main/java/com/twuc/shopping/api/ProductController.java
@@ -1,10 +1,13 @@
 package com.twuc.shopping.api;
 
 import com.twuc.shopping.domain.Product;
+import com.twuc.shopping.dto.ProductDto;
 import com.twuc.shopping.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +19,8 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("/pro/list")
-    public ResponseEntity getRsEventBetween(){
+    @GetMapping("/product")
+    public ResponseEntity getProductList(){
         List<Product> products = productRepository.findAll().stream()
                 .map(item ->Product.builder().name(item.getName())
                         .price(item.getPrice()).image(item.getImage())
@@ -25,4 +28,14 @@ public class ProductController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(products);
     }
+
+    @PostMapping("/product")
+    public ResponseEntity addProductList(@RequestBody Product product){
+        ProductDto productDto = ProductDto.builder().name(product.getName())
+                .price(product.getPrice()).unit(product.getUnit())
+                .image(product.getImage()).build();
+        productRepository.save(productDto);
+        return ResponseEntity.created(null).build();
+    }
+
 }
--- back-end/src/test/java/com/twuc/shopping/api/ProductControllerTest.java
+++ back-end/src/test/java/com/twuc/shopping/api/ProductControllerTest.java
@@ -2,14 +2,21 @@ package com.twuc.shopping.api;
 
 import com.twuc.shopping.dto.ProductDto;
 import com.twuc.shopping.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,6 +30,11 @@ class ProductControllerTest {
     @Autowired
     ProductRepository productRepository;
 
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAll();
+    }
+
     @Test
     public void shouldGetProductList() throws Exception {
 
@@ -32,12 +44,46 @@ class ProductControllerTest {
         productRepository.save(productDto);
 
         mockMvc
-                .perform(get("/pro/list"))
+                .perform(get("/product"))
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].name", is("可乐2")))
                 .andExpect(jsonPath("$[0].price", is("2.50")))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void shouldAddProductWithoutImg() throws Exception {
+
+        String jsonString = "{\"name\":\"可乐1\",\"price\":\"3.50\",\"unit\":\"瓶\"}";
+
+        mockMvc.perform(post("/product").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        List<ProductDto> all = productRepository.findAll();
+        assertNotNull( all );
+        assertEquals(1,all.size());
+        assertEquals("可乐1",all.get(0).getName());
+        assertEquals("3.50",all.get(0).getPrice());
+        assertEquals("瓶",all.get(0).getUnit());
+        }
+
+    @Test
+    public void shouldAddProductWithImg() throws Exception {
+        String img = "https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg";
+
+        String jsonString = "{\"name\":\"可乐1\",\"price\":\"3.50\",\"unit\":\"瓶\",\"image\":\"https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg\"}";
+        mockMvc.perform(post("/product").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        List<ProductDto> all = productRepository.findAll();
+        assertNotNull( all );
+        assertEquals(1,all.size());
+        assertEquals("可乐1",all.get(0).getName());
+        assertEquals("3.50",all.get(0).getPrice());
+        assertEquals("瓶",all.get(0).getUnit());
+        assertEquals(img,all.get(0).getImage());
+
+    }
+
+
 
 
 }
