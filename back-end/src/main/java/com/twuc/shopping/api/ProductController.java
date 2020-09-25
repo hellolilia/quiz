829df217f913ba3000195b4992da8d0385a0a54d
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-26 00:26:57 +0800

    get product from mysql

--- back-end/src/main/java/com/twuc/shopping/api/ProductController.java
+++ back-end/src/main/java/com/twuc/shopping/api/ProductController.java
@@ -5,15 +5,13 @@ import com.twuc.shopping.dto.ProductDto;
 import com.twuc.shopping.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 
     @Autowired
--- back-end/src/test/java/com/twuc/shopping/api/ProductControllerTest.java
+++ back-end/src/test/java/com/twuc/shopping/api/ProductControllerTest.java
@@ -32,54 +32,46 @@ class ProductControllerTest {
 
     @BeforeEach
     void setUp() {
+        String img = "https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg";
         productRepository.deleteAll();
+        productRepository.save(ProductDto.builder()
+                .name("可乐1").price("1.50").unit("瓶").image(img).build());
+        productRepository.save(ProductDto.builder()
+                .name("可乐2").price("2.50").unit("2瓶").image(img).build());
+        productRepository.save(ProductDto.builder()
+                .name("可乐3").price("3.50").unit("3瓶").image(img).build());
+        productRepository.save(ProductDto.builder()
+                .name("可乐4").price("4.50").unit("4瓶").image(img).build());
+        productRepository.save(ProductDto.builder()
+                .name("可乐5").price("5.50").unit("5瓶").image(img).build());
     }
 
     @Test
     public void shouldGetProductList() throws Exception {
 
-        ProductDto productDto = ProductDto.builder()
-                .name("可乐2").price("2.50").unit("瓶").build();
-
-        productRepository.save(productDto);
-
         mockMvc
                 .perform(get("/product"))
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].name", is("可乐2")))
-                .andExpect(jsonPath("$[0].price", is("2.50")))
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andExpect(jsonPath("$[4].name", is("可乐5")))
+                .andExpect(jsonPath("$[4].price", is("5.50")))
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void shouldAddProductWithoutImg() throws Exception {
-
-        String jsonString = "{\"name\":\"可乐1\",\"price\":\"3.50\",\"unit\":\"瓶\"}";
-
-        mockMvc.perform(post("/product").content(jsonString).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
-        List<ProductDto> all = productRepository.findAll();
-        assertNotNull( all );
-        assertEquals(1,all.size());
-        assertEquals("可乐1",all.get(0).getName());
-        assertEquals("3.50",all.get(0).getPrice());
-        assertEquals("瓶",all.get(0).getUnit());
-        }
 
     @Test
     public void shouldAddProductWithImg() throws Exception {
         String img = "https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg";
 
-        String jsonString = "{\"name\":\"可乐1\",\"price\":\"3.50\",\"unit\":\"瓶\",\"image\":\"https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg\"}";
+        String jsonString = "{\"name\":\"可乐6\",\"price\":\"6.50\",\"unit\":\"6瓶\",\"image\":\"https://img.zcool.cn/community/017483591da785b5b3086ed4904903.jpg@1280w_1l_2o_100sh.jpg\"}";
         mockMvc.perform(post("/product").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
         List<ProductDto> all = productRepository.findAll();
         assertNotNull( all );
-        assertEquals(1,all.size());
-        assertEquals("可乐1",all.get(0).getName());
-        assertEquals("3.50",all.get(0).getPrice());
-        assertEquals("瓶",all.get(0).getUnit());
-        assertEquals(img,all.get(0).getImage());
+        assertEquals(6,all.size());
+        assertEquals("可乐6",all.get(5).getName());
+        assertEquals("6.50",all.get(5).getPrice());
+        assertEquals("6瓶",all.get(5).getUnit());
+        assertEquals(img,all.get(5).getImage());
 
     }
 
