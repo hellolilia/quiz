b336feed32cba9ba5e8926502d119bf30e09309e
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-26 00:26:08 +0800

    get product from mysql

--- front-end/src/components/Mall.js
+++ front-end/src/components/Mall.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import '../styles/Mall.css'
+import Products from "./Products";
 
 class Mall extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            products: []
+        }
+    }
+    getData(){
+        fetch('http://localhost:8080/product',{
+            method:'GET',
+            mode: 'cors',
+        }).then(res=>res.json()).then(
+            data=>{
+                console.log(data);
+                this.setState({
+                    products :data})
+                })
 
-
+    }
+    componentDidMount(){
+        this.getData();
+    }
     render() {
         return (
-            <div className='home'>
-                <p>This is a beautiful mall Page.</p>
-            </div>
-        );
-    }
+            <div className='mall'>
+            <React.Fragment>
+                <ul>
+                    {this.state.products.map( item =>{
+                        return (
+                            <li key={item}>
+                                <Products
+                                    name={item.name}
+                                    price={item.price}
+                                    unit={item.unit}
+                                    image={item.image}
+                                />
+                            </li>)
+                    })}
+                </ul>
 
+            </React.Fragment>
+            </div>
+        )
+    }
 }
 
 export default Mall;
--- front-end/src/components/Products.js
+++ front-end/src/components/Products.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../styles/Products.css'
-import productImagePlaceholder from '../assets/product_image_placeholder.png';
 import addProductImage from '../assets/AddProduct.png'
 
 const Products = ({name,price,unit,image}) => {
 
     return (
         <div className="product">
-            <img className='productImg' src={productImagePlaceholder || {image}} alt='product'/>
+            <img className='productImg' src={image} alt='product'/>
             <p className="productName">{name}</p>
             <p className='productPrice'>单价：{price}元/{unit}</p>
             <button> <img src={addProductImage} alt='add'/></button>
